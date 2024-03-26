1e42756d5f396ad1f7251bbb27c4b4dd931dfea3
Florian Sanders <florian.sanders@clever-cloud.com>
2024-03-26 12:44:57 +0100

    test gh action with nix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
           name = finalAttrs.pname;
           src = ./.;
           rev = "be0db0115a1f2cf2899e67517b9fcb7c3e1f6edc";
-          npmDepsHash = "sha256-8dbbHJFcgSh0cQkVp1BMwNmf4knCQlqRXbshnKVXQdA=";
+          npmDepsHash = "sha256-a5OncYj7YSiDGBbURNUxTP2r2DT	oto";
           dontNpmBuild = true;
         };
 
