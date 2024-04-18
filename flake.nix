9e954f5628b674f6876910766ac3583636b9cb77
Florian Sanders <florian.sanders@clever-cloud.com>
2024-04-18 15:41:56 +0200

    new workflows

--- flake.nix
+++ flake.nix
@@ -27,6 +27,12 @@
 
         dontNpmBuild = true;
 
+        postInstall = ''
+          mkdir -p $out/share/{bash-completion/completions,zsh/site-functions}
+          $out/bin/clever --bash-autocomplete-script $out/bin/clever > $out/share/bash-completion/completions/clever
+          $out/bin/clever --zsh-autocomplete-script $out/bin/clever > $out/share/zsh/site-functions/_clever
+        '';
+
         meta = {
           description = "";
           homepage = "";
