0389a1eec4d63ae8f5053b049d3158c89178d45a
Florian Sanders <florian.sanders@clever-cloud.com>
2024-05-22 07:47:05 +0000

    new release v3.6.0

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         default = pkgs.buildNpmPackage rec {
           pname = "clever-tools";
 
-          version = "3.5.1";
+          version = "3.6.0";
 
           nodejs = pkgs.nodejs-18_x;
 
@@ -33,10 +33,10 @@
             owner = "CleverCloud";
             repo = "clever-tools";
             rev = "${version}";
-            hash = "sha256-k3k8PoJVnTDExz2MbuEPTnIygNj8JxPw7t6SOQWNsGY=";
+            hash = "sha256-3LZ04YeVrKXa9rk3Wh4yS4zk4cUyT0fw7VnsaPXdKgs=";
           };
 
-          npmDepsHash = "sha256-quNBAWC1wAsIVcky7H8mYlw2sJkndVSbnlyRaJGndT0=";
+          npmDepsHash = "sha256-dJpe+YgklzuwBF1FnTQxBCiIBF2gaG9MIp0cfRl+YfQ=";
 
           dontNpmBuild = true;
 
